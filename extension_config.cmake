e57eeb1af55b460a3e24e91dda839c4a1385066f
Joachim Rosskopf <jr@data-zoo.de>
2024-06-09 13:35:26 +0200

    Fix build.

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,15 +1,12 @@
 if(CMAKE_BUILD_TYPE STREQUAL "Release")
     duckdb_extension_load(erpl_web
         SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
+        LOAD_TESTS
         DONT_LINK)
-    set(BUILD_UNITTESTS OFF)
 else()
     duckdb_extension_load(erpl_web
         SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
         LOAD_TESTS)
-    set(BUILD_UNITTESTS ON)
 endif()
-
-
 
 duckdb_extension_load(json)
